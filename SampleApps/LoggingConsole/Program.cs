952c4820e569610220ae5031e5964bd0057dc5b6
Vikrant <vikrant460@gmail.com>
2024-10-23 15:06:49 +0530

    start fresh

--- SampleApps/LoggingConsole/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Logging.Core;
-
-Console.WriteLine("Hello, World!");
-var errorDetails = new LogDetail { Error = "Some error happened" };
-Logger.LogError(errorDetails);
-Console.Read();
